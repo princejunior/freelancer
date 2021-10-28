fee4e632adb07f2dbb8412dce50761e872a6770c
princejunior <47012681+princejunior@users.noreply.github.com>
2021-10-28 17:29:28 -0600

    added User class

---
+++ app/src/main/java/com/example/freelance/User.java
@@ -0,0 +1,13 @@
+package com.example.freelance;
+
+import java.util.ArrayList;
+
+public class User {
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String about_me_description;
+
+    private ArrayList cart;
+
+}
