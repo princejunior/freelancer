f4fc9bcdb1f75b596b89856b05a3f64a30769d62
princejunior <47012681+princejunior@users.noreply.github.com>
2021-10-29 19:03:06 -0600

    added bottom navigation view. The fragments are overlaying eachother when a nav button is clicked on.

--- app/src/main/java/com/example/freelance/MainActivity.java
+++ app/src/main/java/com/example/freelance/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.freelance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.fragment.app.Fragment;
+//import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,32 +27,73 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.firstFragment:
+                            selectedFragment = new FirstFragment();
+                            break;
+                        case R.id.secondFragment:
+                            selectedFragment = new SecondFragment();
+                            break;
+                        case R.id.thirdFragment:
+                            selectedFragment = new ThirdFragment();
+                            break;
+                    }
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.fragmentView, selectedFragment, null)
+                            .setReorderingAllowed(true)
+                            .commit();
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentView,selectedFragment).commit();
+                    return true;
+                }};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this,"Firebase connection Success", Toast.LENGTH_LONG).show();
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(navListener);
 
-        myRef.setValue("Hello, World!");
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(null, "Value is: " + value);
-            }
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        fragmentManager.beginTransaction()
+//                .replace(R.id.fragmentView, FirstFragment.class, null)
+//                .setReorderingAllowed(true)
+//                .commit();
+//        NavController navController = Navigation.findNavController(R.id.fragmentView);
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(null, "Failed to read value.", error.toException());
-            }
-        });
+//        bottomNavigationView.setup
+//        AppBarConfiguration appBarConfiguration = AppBarConfiguration(setOf(R.id.firstFragment, R.id.secondFragment, R.id.thirdFragment));
+
+//        Toast.makeText(MainActivity.this,"Firebase connection Success", Toast.LENGTH_LONG).show();
+//
+//        // Write a message to the database
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        myRef.setValue("Hello, World!");
+//        // Read from the database
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(null, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(null, "Failed to read value.", error.toException());
+//            }
+//        });
     }
+
 }
